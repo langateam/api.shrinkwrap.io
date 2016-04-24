2c9aa80cea64238b5fc44c7470c3622e3b4608d2
Travis <me@traviswebb.com>
2016-04-24 00:35:45 -0400

    [yo] initial commit

---
+++ config/database.js
@@ -0,0 +1,25 @@
+/**
+ * Database Configuration
+ * (app.config.database)
+ *
+ * Configure the ORM layer, connections, etc.
+ *
+ * @see {@link http://trailsjs.io/doc/config/database}
+ */
+module.exports = {
+
+  /**
+   * Define the database stores. A store is typically a single database.
+   *
+   * Use the SQLite3 by default for development purposes.
+   *
+   * Set production connection info in config/env/production.js
+   */
+  stores: {
+
+  },
+
+  models: {
+    migrate: 'alter'
+  }
+}
